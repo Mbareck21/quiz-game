e3a3c8a563785533cb8571ea80c386a24559a0cc
Mbareck21 <missara.lamine@gmail.com>
2023-03-26 13:09:38 -0500

    onHold function

--- src/App.js
+++ src/App.js
@@ -2,40 +2,73 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import blobOne from "./Images/blob1.png";
 import blobTwo from "./Images/blob2.png";
-import Quiz from "./Quiz";
+import QuizQuestion from "./QuizQuestion";
+import { nanoid } from "nanoid";
+
 const APIurl =
   "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple";
 function App() {
-  const [quiz, setQuiz] = useState(false);
+  const [start, setStart] = useState(false);
   const [data, setData] = useState([]);
-  const startQuiz = () => {
-    console.log("quiz started");
-    setQuiz(true);
-  };
+  const [selectedAnswer, setSelectedAnswer] = useState(false);
+
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const storedData = JSON.parse(localStorage.getItem("data"));
+    if (storedData) {
+      setData(storedData);
+    } else {
       fetch(APIurl)
         .then((res) => res.json())
-        .then((res) => setData(res.results));
-    }, 2000);
-    return () => clearTimeout(timer);
+        .then((res) => {
+          const response = res.results.map((element) => {
+            let answers = [...element.incorrect_answers];
+            answers.push(element.correct_answer);
+            return {
+              ...element,
+              answers: answers,
+              id: nanoid(),
+              isHeld: false,
+            };
+          });
+          setData(response);
+          localStorage.setItem("data", JSON.stringify(response));
+        });
+    }
   }, []);
-  console.log(data[0]);
+
+  console.log("dataElements", data);
+  const holdAnswer = (id, index) => {
+    const updatedData = data.map((element) => {
+      return element.id === id
+        ? { ...element, isHeld: !element.isHeld }
+        : element;
+    });
+    setSelectedAnswer(index);
+    setData(updatedData);
+    localStorage.setItem("data", JSON.stringify(updatedData));
+  };
+  console.log("updatedData", data);
   return (
     <main className="App">
       <img className="blobTop" src={blobOne} alt="" />
-      {quiz ? (
-        <Quiz data={data} />
+
+      {start ? (
+        <QuizQuestion
+          data={data}
+          onHold={holdAnswer}
+          selectedAnswer={selectedAnswer}
+        />
       ) : (
         <>
           <h1>Quizzical!</h1>
 
           <p className="gameDescription">description</p>
-          <button onClick={startQuiz} className="startButton">
+          <button onClick={() => setStart(true)} className="startButton">
             Start quiz
           </button>
         </>
       )}
+
       <img className="blobBottom" src={blobTwo} alt="" />
     </main>
   );
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,24 +1,30 @@
 import React from "react";
-function QuizQuestion({ data }) {
+function QuizQuestion({ data, onHold, selectedAnswer }) {
   return (
     <div className="questionElementContainer">
-      <ul>
-        {data.map((question) => (
-          <li key={question.correct_answer}>
-            <p className="questionText">{question.question}</p>
-            <div className="answersList">
-              {question.incorrect_answers.map((answer, idx) => {
-                return (
-                  <span className="answersListItem" key={idx}>
-                    {answer}
-                  </span>
-                );
-              })}
-              <span className="answersListItem">{question.correct_answer}</span>
-            </div>
+      <ul className="elementList">
+        {data.map((element) => (
+          <li key={element.id} className="elementListItem">
+            <h4 className="elementQuestion">{element.question}</h4>
+            <ul className="elementAnswerList">
+              {element.answers.map((answer, index) => (
+                <li
+                  key={index}
+                  onClick={() => onHold(element.id, index)}
+                  className={
+                    element.isHeld && selectedAnswer === index
+                      ? "elementAnswerListItemHeld"
+                      : "elementAnswerListItem"
+                  }
+                >
+                  {answer}
+                </li>
+              ))}
+            </ul>
           </li>
         ))}
       </ul>
+      <button className="checkButton">Check Answers</button>
     </div>
   );
 }
